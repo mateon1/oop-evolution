277e55d4f61385c2b2890483afcb1f0a550ab239
Mateusz Naściszewski <matin1111@wp.pl>
2019-12-21 17:37:53 +0100

    Config file can be specified on the command line

--- src/agh/cs/evolution/Main.java
+++ src/agh/cs/evolution/Main.java
@@ -2,15 +2,35 @@ package agh.cs.evolution;
 
 import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) throws ParseException {
-        SimParameters params = SimParameters.fromJson(
-                "{\"mapSize\": [100, 30], \"jungleSize\": [10, 10],"
-                        + " \"startEnergy\": 50, \"plantEnergy\": 25, \"moveEnergy\": 1,"
-                        + " \"startAnimals\": 25, \"grassTurns\": 50}"
-        );
+        SimParameters params;
+        if (args.length == 0) {
+            params = SimParameters.fromJson(
+                    "{\"mapSize\": [100, 30], \"jungleSize\": [10, 10],"
+                            + " \"startEnergy\": 50, \"plantEnergy\": 25, \"moveEnergy\": 1,"
+                            + " \"startAnimals\": 25, \"grassTurns\": 50}"
+            );
+        } else {
+            try {
+                String config = new String(Files.readAllBytes(Paths.get(args[0])));
+                params = SimParameters.fromJson(config);
+            } catch (IOException e) {
+                System.err.println("Could not read config file (" + args[0] + ")");
+                e.printStackTrace();
+                System.exit(1);
+                // javac doesn't know that System.exit doesn't return, so
+                // doesn't know that params is always set without this
+                return;
+            }
+        }
         int seed = 0;
 
+        System.out.println("Using seed: " + seed);
         WorldMap w = new WorldMap(params, seed);
 
         System.out.println("Growing grass for a bit");
@@ -24,12 +44,10 @@ public class Main {
         System.out.println("Running the world!");
         for (int i = 0; i < 100000; i++) {
             w.tick();
-            if (i % 10 == 0) {
-                w.visualize();
-                try {
-                    Thread.sleep(10, 0);
-                } catch (InterruptedException ignored) {
-                }
+            w.visualize();
+            try {
+                Thread.sleep(10, 0);
+            } catch (InterruptedException ignored) {
             }
         }
 
